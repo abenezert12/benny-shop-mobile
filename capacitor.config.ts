a75963ebef7ae49f998eeace08c5ad31532022ce
abenezert12 <abenezertekalign12@gmail.com>
2026-05-10 18:15:14 +0300

    update: ui and fixed bugs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.bennyshopmobile',
-  appName: 'benny-shop-mobile',
+  appId: 'com.bennyshop.mobile',
+  appName: 'Benny Shop',
   webDir: 'dist'
 };
 
